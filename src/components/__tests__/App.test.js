cc16a43c400fbabf2a9ccd87e768ee8c7f071729
gershon <gershon@cio.gov.il>
2018-07-03 17:26:53 +0300

    2

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from '../App';
+
+it('show the test',() => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />,div);
+    expect(div.innerHTML).toContain('Comment Box');
+    ReactDOM.unmountComponentAtNode(div);
+});
