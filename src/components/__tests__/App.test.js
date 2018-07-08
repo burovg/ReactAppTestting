a0eebe034a05d1354adcec55f1f04b9e2a583a9b
gershon <gershon@cio.gov.il>
2018-07-08 16:57:52 +0300

    0807_1

--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import {shallow} from 'enzyme';
-import App from '../App';
-import CommentBox from '../CommentBox';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
+
 
 //import App from '../App';
 
+let wrapper;
+
+beforeEach(() => {
+    wrapper = shallow(<App />);
+});
+
 it('show the test',() => {
-    const wrapper = shallow(<App />);
-    expect(wrapper.find(CommentBox).length).toEqual(111);
+    expect(wrapper.find(CommentBox).length).toEqual(1);
+});
+
+it('show comment list',() => {
+    expect(wrapper.find(CommentList).length).toEqual(1);
 });
